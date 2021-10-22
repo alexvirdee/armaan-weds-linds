6ed970740ec605d99103a293ca040d614e1d9ea5
Alexinder Virdee <alex.virdee08@gmail.com>
2021-10-22 17:34:49 -0400

    Configuring gatsby + contentful

--- src/pages/photos.js
+++ src/pages/photos.js
@@ -1,60 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Navbar from "../components/layout/Navbar";
 import styled from "styled-components"
 import "@fontsource/alex-brush"
 
-const query = `
-{
-    lindsayWeddingCollection {
-    items {
-      title
-      image {
-        url
-      }
-    }
-  }
-}
-`
 
 const Photos = () => {
-    const [page, setPage] = useState(null);
-
-    useEffect(() => {
-        window
-          .fetch(``, {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              // Authenticate the request
-              Authorization: "Bearer ",
-            },
-            // send the GraphQL query
-            body: JSON.stringify({ query }),
-          })
-          .then((response) => response.json())
-          .then(({ data, errors }) => {
-            if (errors) {
-              console.error(errors);
-            }
-    
-            // rerender the entire component with new data
-            setPage(data.lindsayWeddingCollection.items[0]);
-          });
-
-          console.log(page)
-
-      }, []);
-
-    if (!page) {
-        return "Loading....";
-    }
-
     return (
         <>
         <Navbar />
         <Title>Photos</Title>
-        <img src={page.image.url} />
-        <p>{page.title}</p>
         </>
     )
 }
---
+++ templates/gallery-item.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { graphql } from "gatsby";
+
+const GalleryItem = ({ data }) => {
+    <div>
+        <h1>Gallery Page</h1>
+        <p>View images here</p>
+    </div>
+}
+
+export default GalleryItem;

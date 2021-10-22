7c24c16b22e486f664cc2c97af0a17de25911b63
Alexinder Virdee <alex.virdee08@gmail.com>
2021-10-22 19:31:44 -0400

    Gatsby Link to slug

--- src/pages/photos.js
+++ src/pages/photos.js
@@ -1,45 +1,44 @@
-import React, { useEffect } from "react";
-import Navbar from "../components/layout/Navbar";
+import React, { useEffect } from "react"
+import Navbar from "../components/layout/Navbar"
 import styled from "styled-components"
 import "@fontsource/alex-brush"
-import { graphql } from "gatsby"
-
+import { graphql, Link } from "gatsby"
 
 const Photos = ({ data }) => {
-    useEffect(() => {
-        console.log("DATA: ", data)
-    }, [data])
+  useEffect(() => {
+    console.log("DATA: ", data)
+  }, [data])
 
-    return (
-        <>
-        <Navbar />
-        <Title>Photos</Title>
-        {data.photo.nodes.map((item) => {
-            return (
-
-                <img src={item.image.file.url} />
-            )
-        })}
-        </>
-    )
+  return (
+    <>
+      <Navbar />
+      <Title>Photos</Title>
+      {data.photo.nodes.map(item => {
+        return <Link to={item.slug}>
+            <img  src={item.image.file.url} />
+        </Link> 
+      })}
+    </>
+  )
 }
 
 const Title = styled.h1`
-    font-family: "Alex Brush";
+  font-family: "Alex Brush";
 `
 
-export default Photos;
+export default Photos
 
 export const query = graphql`
-    query PhotosQuery {
-        photo: allContentfulGalleryPost {
-            nodes {
-                image {
-                    file {
-                        url
-                    }
-                }
-            }
+  query PhotosQuery {
+    photo: allContentfulGalleryPost {
+      nodes {
+        image {
+          file {
+            url
+          }
         }
+        slug
+      }
     }
+  }
 `

799e45cc36e6674560d30fe51f190b0737487c39
Alexinder Virdee <alex.virdee08@gmail.com>
2021-11-20 00:59:49 -0500

    Remove unused component

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,6 @@ import FlowerVector from "../components/layout/Flower-vector";
 import Navbar from "../components/layout/Navbar"
 import Hero from "../components/Hero/Hero";
 import Footer from "../components/layout/Footer";
-// import RSVPForm from "../components/RSVP/RSVPForm";
-// import BestFriends from "../components/BridesAndGrooms/BestFriends";
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`

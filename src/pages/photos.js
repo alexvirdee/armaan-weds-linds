84f30fe3d9abc585acf9fefd61ab83471472c6ea
Alexinder Virdee <alex.virdee08@gmail.com>
2021-11-23 19:27:58 -0500

    Minor hover effects

--- src/pages/photos.js
+++ src/pages/photos.js
@@ -31,25 +31,30 @@ const Title = styled.h1`
 
 const Gallery = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(275px, 1fr));
   grid-gap: 0px;
   place-items: center;
 `
 
 const GalleryImg = styled(GatsbyImage)`
-  max-width: 85%;
+  max-width: 90%;
   height: auto;
   margin: 10px;
   padding: 2%;
   box-shadow: rgba(0, 0, 0, 0.15) 0px 2px 8px;
 
   :hover {
-    -webkit-filter: brightness(85%);
+    -webkit-filter: brightness(95%);
     -webkit-transition: all 10ms ease;
     -moz-transition: all 10ms ease;
     -o-transition: all 10ms ease;
     -ms-transition: all 10ms ease;
     transition: all 10ms ease;
+    box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+  }
+
+  @media (max-width: 768px) {
+    margin-left: 25px;
   }
 `
 
--- src/templates/gallery-item.js
+++ src/templates/gallery-item.js
@@ -95,7 +95,13 @@ const FramedImg = styled(GatsbyImage)`
   display: block;
   padding: 2%;
   margin-left: 45px;
-  box-shadow: rgba(0, 0, 0, 0.15) 0px 2px 8px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+
+  :hover {
+    background-color: #aadaff;
+    box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+  }
 
   @media (max-width: 768px) {
     width: 80%;

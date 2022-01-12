f7b28c3071ae92f6e8f1f2c5471000957ce15fca
Alexinder Virdee <alex.virdee08@gmail.com>
2022-01-11 20:34:09 -0500

    Remove rsvp form didn't mean to add to prod

--- src/pages/RSVP.js
+++ src/pages/RSVP.js
@@ -28,14 +28,6 @@ const RSVP = () => {
                 alt={"rsvp"}
                 width={600}
               />
-              <div>
-                <form>
-                  <label>Reservation Code</label>{" "}
-                  <input placeholder={"Enter your reservation code"} />
-                  <label>Able to come?</label>{" "}
-                  <input placeholder={"Yes or no"} />
-                </form>
-              </div>
               <CloseBtn to={closeTo}>Close</CloseBtn>
             </RSVPContainer>
           ) : (
